a906de0ae32f490536b3638d49d4a6e8b8c2eab6
chloemk <minsung.chloe.kim@gmail.com>
2023-01-23 22:25:53 +0900

    feat: add design

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,8 +11,91 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('home screen'),
+      backgroundColor: const Color(0xFFEEE1D1),
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              _TopPart(),
+              _BottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'D-Day',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 80.0,
+              fontFamily: 'parisienne',
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30.0,
+                  fontFamily: 'sunflower',
+                ),
+              ),
+              Text(
+                '2023.01.20',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                  fontFamily: 'sunflower',
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            'D + 1',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+              fontSize: 50.0,
+              fontFamily: 'sunflower',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'asset/img/middle_image.png',
       ),
     );
   }
