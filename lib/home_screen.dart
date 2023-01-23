a4446c77d9890e69b6d1ae5d0b1942463e0832d4
chloemk <minsung.chloe.kim@gmail.com>
2023-01-23 20:51:15 +0900

    todo: add home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text('home screen'),
+      ),
+    );
   }
 }
